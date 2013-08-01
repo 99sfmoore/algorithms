90e6a047689d16397a5e82af041c93fa2773f042
Sarah Moore <sfmoore99@gmail.com>
2013-08-01 15:04:06 -0400

    Working version

--- scc.rb
+++ scc.rb
@@ -1,67 +1,115 @@
+require 'pry-nav'
+
 class Graph
   attr_reader :graph
 
   class Node
-    attr_accessor :visited, :has_arcs_to, :gets_arcs_from, :fval, :leader
+    attr_accessor :visited, :has_arcs_to, :gets_arcs_from, :fval, :leader, :name
 
-    def initialize
+    def initialize(name)
+      @name = name
       @visited = false
       @gets_arcs_from = []
       @has_arcs_to = []
+      @fval = 0
     end
   end
 
   def initialize(filename)
+    @size = 0
     longstring = File.read(filename)
     graph_array = longstring.split("\n")
-    @graph = Hash.new(nil)
-    graph_array.each do |node|
-      node_list = node.split(" ")
-      node_list.map!{|x| x.to_i}
-      start_node, end_node = node_list.first, node_list.last
-      @graph[start_node] = @graph[start_node] || Node.new
-      @graph[start_node].has_arcs_to << end_node
-      @graph[end_node] = @graph[end_node] || Node.new
-      @graph[end_node].gets_arcs_from << start_node
+    graph_array.each do |line|
+      node_list = line.split(" ")
+      start, finish = node_list.first, node_list.last
+      start_node = instance_variable_get("@node#{start}") || Node.new(start)
+      end_node = instance_variable_get("@node#{finish}") || Node.new(finish)
+      start_node.has_arcs_to << finish
+      end_node.gets_arcs_from << start
+      instance_variable_set("@node#{start}",start_node)
+      instance_variable_set("@node#{finish}",end_node)
+      @size = start.to_i
     end
+    @ordered_nodes = Hash.new
   end
 
-  def reverse_dfs_loop
+  def dfs_loop(first_iter = true)
     @num_processed = 0
-    @graph.each do |node_name,node| 
-      unless node.visited
-        reverse_dfs(node)
+    @source_vertex = nil
+    @stack = []
+    @size.downto(1) do |n|
+      #puts "looking at #{n}"
+      first_iter ? current_node = instance_variable_get("@node#{n}") : current_node = @ordered_nodes[n]
+      
+      unless current_node.visited == first_iter
+        @source_vertex = current_node.name
+        @stack << current_node
+        #binding.pry
+        dfs(current_node,first_iter)
+        #puts "done with dfs"
+        #binding.pry
+        top_node = @stack.pop
+        if first_iter
+          @num_processed +=1
+          #puts "setting fval to #{@num_processed}"
+          top_node.fval = @num_processed
+          @ordered_nodes[@num_processed] = top_node
+        else
+          @gcc_groups[@source_vertex] +=1
+        end
+        until @stack.empty? do
+          #binding.pry
+          next_node = @stack.last
+          dfs(next_node, first_iter)
+          top_node = @stack.pop
+          if first_iter
+            @num_processed +=1
+            top_node.fval = @num_processed
+            @ordered_nodes[@num_processed] = top_node
+          else
+            @gcc_groups[@source_vertex] +=1
+          end
+        end
       end
     end
   end
 
-  def reverse_dfs(start_node)
-    start_node.visited = true
-    start_node.gets_arcs_from.each do |n|
-      puts "Looking at #{n}"
-      current_node = @graph[n]
-      unless current_node.visited
-        reverse_dfs(current_node)
+  def dfs(start_node, reverse=true)
+    start_node.visited = reverse
+    reverse ? arcs = start_node.gets_arcs_from : arcs = start_node.has_arcs_to
+    index = 0
+    forward_path = false
+    #binding.pry
+    while index < arcs.size && !forward_path
+      next_node = instance_variable_get("@node#{arcs[index]}")
+      #puts "going to #{arcs[index]}"
+      if next_node.visited == reverse
+        index +=1 
+      else
+        @stack << next_node
+        forward_path = true
       end
-      puts "path ends at #{n}"
     end
-    @num_processed +=1
-    puts "setting fval to #{@num_processed}"
-    start_node.fval = @num_processed
+    dfs(next_node, reverse) if forward_path
   end
 
-  def dfs_loop
-    current_source_vertex = nil
-    @graph.each
+  def find_gcc
+    @gcc_groups = Hash.new(0)
+    dfs_loop
+    p @ordered_nodes
+    dfs_loop(false)
+    top_5 = Array.new(5){0}
+    @gcc_groups.each do |key, value|
+      top_5.sort!
+      top_5[0] = [value,top_5[0]].max
+    end
+    p top_5.sort.reverse
   end
-
 end
 
 
-my_graph = Graph.new("test_graph.txt")
-puts "before reverse"
-my_graph.reverse_dfs_loop
-puts "after reverse"
-my_graph.graph.each do |k,v|
-  puts "Node#{k}:   #{v.inspect}"
-end
+my_graph = Graph.new("ps4_current.txt")
+my_graph.find_gcc
+
+
+
